52e0a4d01fcb9408b25e4e0455816bd18d7eeb4a
ivstarkov <104798391+ivstarkov@users.noreply.github.com>
2022-07-05 20:34:21 +0400

    1

--- SquareIntoSquare.py
+++ SquareIntoSquare.py
@@ -7,28 +7,29 @@ def decompose(n):
     list = []
     x = n ** 2
     while x > 0:
-    #for _ in range(7):
-        print("======")
-        if math.isqrt(x) ** 2 == x and x > 1:
-            print("+")
+        #print("======")
+        if x == n ** 2 and x > 1:
+            #print("+")
             rest = x - math.isqrt(x - 1) ** 2
             list.append(math.isqrt(x - 1))
-            print(math.isqrt(x - 1))
-            print(rest)
+            #print(math.isqrt(x - 1))
+            #print(rest)
         else:
-            print("-")
+            #print("-")
             rest = x - math.isqrt(x) ** 2
             list.append(math.isqrt(x))
-            print(math.isqrt(x))
-            print(rest) 
+            #print(math.isqrt(x))
+            #print(rest) 
         x = rest
-        print("======")
+        #print("======")
+
+
 
 
 
     list.reverse()
     return list
 
-print(decompose(11))
+print(decompose(50))
 
 #print(11 ** 2 - math.isqrt(11 ** 2 - 1) ** 2)
