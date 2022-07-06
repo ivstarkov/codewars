1e7ca8203a4c54647dcac018c3ef17b84ecf7418
ivstarkov <104798391+ivstarkov@users.noreply.github.com>
2022-07-07 01:57:05 +0400

    1st round

--- snail.py
+++ snail.py
@@ -6,11 +6,32 @@ array = [[1,2,3,1],
 
 width = len(array[0])
 height = len(array)
+
+# 1st row
 list = array[0]
 
-list.append(array[1][2])
-array[2].reverse()
-list.extend(array[2])
-list.append(array[1][0])
-list.append(array[1][1])
+# Right row
+for i in range(1, height - 1):
+    list.append(array[i][width - 1])
+
+# Bottom row
+array[height - 1].reverse()
+list.extend(array[height - 1])
+
+# Left row
+for i in range(height - 2, 0, -1):
+    list.append(array[i][0])
+
+#unpack array
+array.pop(0)
+array.pop()
+for i in range(height - 2):
+    array[i].pop()
+    array[i].pop(0)
+
+
+
+
+
+
 
