47794a35a04eaa4f2d04310d328313363a641d9d
ivstarkov <104798391+ivstarkov@users.noreply.github.com>
2022-07-07 03:15:53 +0400

    almost

--- snail.py
+++ snail.py
@@ -3,35 +3,46 @@ array = [[1,2,3,1],
          [7,8,9,7],
          [7,8,9,7]]
 
+def snail(array):
 
-width = len(array[0])
-height = len(array)
+    width = len(array[0])
+    height = len(array)
 
-# 1st row
-list = array[0]
+    print(array)
+    
+    # 1st row
+    try:
+        list = list.extend(array[0])
+    except:
+        list = array[0]
 
-# Right row
-for i in range(1, height - 1):
-    list.append(array[i][width - 1])
+    # Right row
+    for i in range(1, height - 1):
+        list.append(array[i][width - 1])
 
-# Bottom row
-array[height - 1].reverse()
-list.extend(array[height - 1])
+    # Bottom row
+    array[height - 1].reverse()
+    list.extend(array[height - 1])
 
-# Left row
-for i in range(height - 2, 0, -1):
-    list.append(array[i][0])
+    # Left row
+    for i in range(height - 2, 0, -1):
+        list.append(array[i][0])
 
-#unpack array
-array.pop(0)
-array.pop()
-for i in range(height - 2):
-    array[i].pop()
-    array[i].pop(0)
+    #unpack array
+    array.pop(0)
+    array.pop()
+    for i in range(height - 2):
+        array[i].pop()
+        array[i].pop(0)
 
+    print(list)
+    if not array:
+       return list
 
+    # Recursion
+    print("=========")
+    print(array)
+    snail(array)
+    print("=========")
 
-
-
-
-
+print(snail(array))
--- test.py
+++ test.py
@@ -1,3 +1,5 @@
-list = ["foo", "bar", "welcome"]
-a = [x[::-1] if len(x) >= 5 else x for x in list]
-print(" ".join(a))
+
+if not a:
+    print("empty")
+else:
+    print("noempty")
