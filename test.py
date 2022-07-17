aeca9be81971b1e12b93ab154d64f474683eb504
ivstarkov <104798391+ivstarkov@users.noreply.github.com>
2022-07-18 03:20:50 +0400

    leettest

--- test.py
+++ test.py
@@ -1,20 +1,15 @@
-n = [-6,-3,-2,-1,0,1,3,4,5,7,8,9,10,11,14,15,17,18,19,20]
+def fizzBuzz(n):
+    list = []
+    for i in range(1, n+1):
+        if i % 3 == 0 and i % 5 == 0:
+            list.append("FizzBuzz")
+        elif i % 3 == 0:
+            list.append("Fizz")
+        elif i % 5 == 0:
+            list.append("Buzz")
 
-def solution(args):
-    for x in args:
-        # Find length of consecutive sub-string
-        i = args.index(x)
-        l_counter = 1
-        while i < len(args) - 1 and args[i] == args[i + 1] - 1:
-            l_counter += 1
-            i += 1
-        # Place "-" instead of sub string
-        if l_counter > 2:
-            args[args.index(x) + 1: args.index(x) + l_counter - 1] = "-"
+        else:
+            list.append(str(i))
+    return list
 
-               
-        
-    temp = ','.join(str(x) for x in args)
-    return temp.replace(",-,", "-")
-
-print(solution(n))
+print(fizzBuzz(15))
