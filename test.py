ac89b4b8ae1502037cf248ca53aa3c04add1e13c
ivstarkov <104798391+ivstarkov@users.noreply.github.com>
2022-08-04 22:34:00 +0400

    new taask

--- test.py
+++ test.py
@@ -1,15 +1,36 @@
-def fizzBuzz(n):
-    list = []
-    for i in range(1, n+1):
-        if i % 3 == 0 and i % 5 == 0:
-            list.append("FizzBuzz")
-        elif i % 3 == 0:
-            list.append("Fizz")
-        elif i % 5 == 0:
-            list.append("Buzz")
+import itertools
 
-        else:
-            list.append(str(i))
-    return list
+def get_pins(observed):
+    
+    # Dict of possible alternative digits
+    keydict = {"1": "124",
+               "2": "1235",
+               "3": "236",
+               "4": "1457",
+               "5": "24568",
+               "6": "3569",
+               "7": "478",
+               "8": "57890",
+               "9": "689",
+               "0": "08"}
+    
+    # List of all combinations of pin length
+    pinlist = list(itertools.combinations_with_replacement("1234567890", len(observed)))
+    
+    # Drop item of list if it is not in possible alternatives   
+    for i in range(len(observed)):
+        print(pinlist)
+        for pin in pinlist:
+            print(pin)
+            print(keydict[observed[i]])
+            
+            if pin[i] not in keydict[observed[i]]:
+                print("=======")
+                print("drop" + str(pin))
+                print("=======")
+            #    pinlist.remove(pin)
+           
+    return pinlist
 
-print(fizzBuzz(15))
+
+print(get_pins("8"))
