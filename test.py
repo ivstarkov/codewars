2c0bdb95f961f69fa4cfdf011f2611fbf3df6c41
ivstarkov <104798391+ivstarkov@users.noreply.github.com>
2022-08-06 15:15:43 +0400

    should figure out

--- test.py
+++ test.py
@@ -1,10 +1,15 @@
-import itertools
+def permutations(string):
+  if len(string) == 1: return set(string)
+  first = string[0]
+  rest = permutations(string[1:])
+  result = set()
+  for i in range(0, len(string)):
+    for p in rest:
+      result.add(p[0:i] + first + p[i:])
+  return result
 
-def permutations(s):
-    print(s)
-    print(list(itertools.combinations("ab", 2)))
+
+print(permutations("ab"))
 
 
 
-
-permutations("ab")
